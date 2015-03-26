3202e9d1b0075ec327f77e007f3b89c0cb1f1e0f
aitorpena8 <aitorpena@gmail.com>
2015-03-26 20:56:58 +0100

    Pantalla de Ayuda, separado loadData y para la internacionalizacion i18n

--- js/app/ayuda.js
+++ js/app/ayuda.js
@@ -1,33 +1,52 @@
 var APP = APP || {};
 $(document).ready(function () {
     "use strict";
+    var searchVal = "";
+    var selCont = "0";
 
+    var trashi18nTextArr = [];
+
+    function updateTable(e) {
+        searchVal = $('#searchTrashById').val();
+        //console.log(searchVal);
+        selCont = $('#selectContainer').val();
+        $('#tableTrash').empty();
+        fillTable();
+        $('#alpIndex').empty();
+        fillIndex();
+
+
+    }
 
     function fillAutoComplete() {
         console.log("fill autocomplete");
 
-        var availableTags = Object.keys(APP.data.trashContainer);
 
         $("#searchTrashById").autocomplete({
-            source: availableTags
+            source: trashi18nTextArr,
+            select: function (event, ui) {
+                console.log('select');
+                searchVal = ui.item.value;
+                console.log(searchVal);
+                updateTable();
+            }
         });
     }
 
-    function updateTable(e) {
-        console.log("change");
 
-
-    }
 
     function fillIndex() {
         var alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
         for (var i in alp) {
-
+            var anchor = document.getElementById(alp[i] + 'Ind');
+            var dis = ''
+            if (!anchor)
+                dis = "disabled='disabled'";
             var $li = $('<li/>', {
-                html: '<a href=#' + alp[i] + '>' + alp[i] + '</a>'
-            });
+                html: '<a   href=#' + alp[i] + ' ' + dis + '  disabled= "disabled">' + alp[i] + '</a>',
 
+            });
             $('#alpIndex').append($li);
 
 
@@ -37,57 +56,76 @@ $(document).ready(function () {
 
     function fillSelect() {
         console.log("fill select");
-
-
-
-
-
         for (var container in APP.data.containerTrash) {
 
 
 
             var $tr = $('<option/>', {
-                html: container
+                html: APP.language.getText(container),
+                value: container
             });
 
             $('#selectContainer').append($tr);
         }
-
-
     }
 
     function fillTable() {
         console.log("fill table");
-        var currentLetter, oldLetter;
+        var currentLetter, oldLetter = '';
 
 
         var c = 0;
         for (var trash in APP.data.trashContainer) {
             //for (var i=0; i<APP.data.trashContainer.length;i++) {
 
-            if (c === 0) {
-                oldLetter = trash[0];
+            /*   if (c === 0) {
+                   oldLetter = trash[0];
+               }*/
 
-            }
+
 
             currentLetter = trash[0];
+            var trashi18nText = APP.language.getText(trash);
+            var containers = APP.data.trashContainer[trash].containers;
+
+
+            if (trashi18nText.indexOf(searchVal) !== -1 && (selCont === "0" || containers.indexOf(selCont) !== -1)) {
 
 
 
-            var $tr = $('<tr/>', {
-                html: '<td>' + trash + '</td><td>' + APP.data.trashContainer[trash].containers.join(',') + '</td>'
-            });
+                var containersText = '';
+                var n = 0
+                for (var i in containers) {
+                    var container = containers[i];
+                    var str = APP.language.getText(container);
+                    if (n === 0)
+                        containersText += str;
+                    else {
+                        containersText += ',' + str;
 
+                    }
+                    n++;
 
-            if (currentLetter !== oldLetter) {
-                var $a = $('<a/>', {
-                    name: currentLetter
+                }
+
+                trashi18nTextArr.push(trashi18nText);
+
+                var $tr = $('<tr/>', {
+                    html: '<td>' + trashi18nText + '</td><td>' + containersText + '</td>'
                 });
-                $tr.append($a);
-                oldLetter = currentLetter;
-            }
 
-            $('#tableTrash').append($tr);
+
+                if (currentLetter !== oldLetter) {
+                    var $a = $('<a/>', {
+                        name: currentLetter,
+                        id: currentLetter + 'Ind'
+                    });
+                    $tr.append($a);
+                    oldLetter = currentLetter;
+                }
+
+                $('#tableTrash').append($tr);
+            }
             c++;
         }
 
@@ -98,7 +136,10 @@ $(document).ready(function () {
     fillAutoComplete();
     fillSelect();
 
+    $('#searchTrashById').on('keyup', updateTable);
     $('#searchTrashById').on('change', updateTable);
+    $('#selectContainer').on('change', updateTable);
+
 
 
 
