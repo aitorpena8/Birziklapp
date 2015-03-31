ff8f907edb21503e61c0d1bba2b0d72dd3beab46
aitorpena8 <aitorpena@gmail.com>
2015-03-31 19:37:12 +0200

    Pantaila de consulta con aspecto finalizado, fichero de datos cambiados de .json a .js etc.

---
+++ js/app/data/trashContainer_min.js
@@ -0,0 +1,1460 @@
+var APP = APP || {};
+APP.data = APP.data || {};
+APP.data.trashContainer = {};
+APP.data.trashContainer ={
+	"achicoria": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"algas": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"alpiste": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"bolsa_de_infusion": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"caramelos": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"carne": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"cascara_de_huevos": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"cascara_de_marisco": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"cereales": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"comida_de_animales_domesticos": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"embutido": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"espinas": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"filtro_de_cafede_papel": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"frutos_secos": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"hueso": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"huesos_de_aceitunas": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"infusiones": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"mantequilla": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"marisco": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"mayonesa": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"mermelada": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"paja": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"palillo": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"panuelos_de_papel": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"peladuras_de_fruta_y_verdura": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"pelo": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"pescado__espina_de_pescado": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"piel_de_limon": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"piel_de_naranja": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"pluma": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"poso_de_cafe": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"queso": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"restos_de_comida": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"serri­n": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"te": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"verduras": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"virutas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"yogures_y_lacteos": {
+		"containers": [
+			"contenedor_de_organico"
+		]
+	},
+	"agenda": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"antifaz_de_papel": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"archivador": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"bandeja_de_carton": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"baraja_de_cartas": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"bloc": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"boleto": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"bono": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cajetillas_de_tabaco": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"calendario": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"carpeta_/_carpetas_az": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cartel": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cartulina": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"chequera": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"clasificador": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"comic": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cuaderno": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cuentos": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"cupones": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"diccionario": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"dorsal": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"dossier": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"enciclopedia": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"factura": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"folleto": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"gui­a_telefonica": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"libreta": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"papel": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"papel_de_oficina": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"periodico": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"post-it": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"postal": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"poster": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"puzzle": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"recibo": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"revista": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"rollo_de_papel_higienico": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"talonario": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"ticket": {
+		"containers": [
+			"papel_y_carton"
+		]
+	},
+	"aerosoloes": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"agua_oxigenada": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"alcohol_de_quemar": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"anillas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bandeja_de_aluminio": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bandeja_de_porexpan": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bolsa_de_alimentos": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bolsa_de_aperitivos": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bolsa_de_caramelos": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bolsa_de_congelados": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bolsa_de_plastico": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"bote_de_cola": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"botella_de_plastico": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"brick": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"chapa_de_botella": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"dosificador_de_botella": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"envoltorio_de_alimentacion": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"film_transparente": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"interruptor": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"lata": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"papel_de_aluminio": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"pintalabios": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"recambio_de_ambientador": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"red_de_cebollas_/limones_/_naranjas_/_patatas": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"spray": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tapa_de_recipiente_de_alimento": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tapas_de_botes": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tapon_de_botella": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tarro": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tetrabrik": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tubo_de_pasta_de_dientes_/_productos_cosmeticos": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"tubos_de_productos_de_limpieza": {
+		"containers": [
+			"plastico"
+		]
+	},
+	"abanico": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"abre_botellas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"abrecartas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"abrelatas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"aceitera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"acetato": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"aguja": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"aletas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"alfombrilla_del_raton_del_ordenador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"algodon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"alicates": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"antena": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"antifaz_de_plastico_y/o_yeso": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"anzuelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"arena_de_gatos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"balon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"banda_depilatoria": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"barba": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"baston": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bastonciillos_de_algodon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bastoncillos_higienicos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"betun": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"biberon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bisuteri­a": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"boligrafo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bolsa_de_agua_caliente": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"bolsa_de_aspirador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"botiqui­n": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"boton": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"brasas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"brida": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"brujula": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"candado": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"caramelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"carbon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cartucho_de_caza": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cascanueces": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cazuela": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cello": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"celofan": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cenicero": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"ceniza_fri­a": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cepillo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cepillo_de_dientes": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cepillo_para_el_pelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"ceramica": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cerilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cerradura": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"chicle": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"chupete": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cigarrillos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cinta_de_carrocero": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cinta_de_embalaje": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cinta_de_vi­deo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cintas_de_vi­deo_y_casete": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"clip_/_grapa": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"colgador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"colilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"collar": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"colorete": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"compresas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"condon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cremallera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cubiertos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cuchilla_de_afeitar": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"cutter": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"dado": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"dardo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"dedal": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"dentadura_postiza": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"destornillador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"diadema": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"disco_de_lactancia": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"disco_desmaquillante": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"ensaladera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"escobilla_de_bano": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"escurridor": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"esparadrapo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"esparto": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"espatula": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"esponja": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"espumadera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"esterilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"estropajo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"excremento_animal": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"filtro": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"filtro_de_agua": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"floreros": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"flotador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"fotografi­a": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"gafas_de_buceo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"gancho_de_pelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"gasa": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"golosina": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"goma": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"goma_de_borrar": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"goma_de_pelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"gorro_de_ducha": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"grapa": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"grapadora": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"hielera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"hilo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"hilo_dental": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"imperdible": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"incienso": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"jabon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"jabonera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"jarra": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"jeringuilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"juego_de_mesa": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"latex": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"lentejuelas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"lentes": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"lentilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"lija": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"lima_de_unas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"llave": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"llave_inglesa": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"llavero": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"maleta": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"maquillaje": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"martillo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"mecha": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"metro": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"muelle": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"munequera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"nylon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"palo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"palo_de_caramelo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"papel_plastificado": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"panales": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pedal": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pegatina": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"peine": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"peli­cula_de_fotos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pelota": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"peluca": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pendientes": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pinza_de_depilar": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pinza_de_tender": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pisapapeles": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pita": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pitillera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"pluma_estilografica": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"polvo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"preservativo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"puchero": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"punta_de_lapiz": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"punzon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"raqueta": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"regla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"rotulador": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"rulo": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"sacacorcho": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"sacapuntas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"salero": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"salsera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"salva_slip": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"sarten": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"silbato": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"sopera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"sortija": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tabaco": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tampon": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tarjetas_de_credito": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"taza": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tenaza": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tetina": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tijeras": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tipp-ex": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tirita": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tiza": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"toallita_limpiadora": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"toallitas": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"tornillos": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"varilla": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"vasos_de_cristal": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"vela": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"venda": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"vinagrera": {
+		"containers": [
+			"rechazo"
+		]
+	},
+	"botella_de_vidrio": {
+		"containers": [
+			"vidrio"
+		]
+	}
+}
--- js/app/i18n.js
+++ js/app/i18n.js
@@ -2,43 +2,63 @@ var APP = APP || {};
 
 $(document).ready(function () {
     "use strict";
-    APP.language = APP.language || {};
-
-
-
+    APP.language = {};
 
     APP.language = (function load() {
         var text = APP.language.text || {};
-        var code = APP.language.code;
-        var langText={};
-        if (!code) {
-            APP.language.code = 'es';
-            code = 'es';
-        }
+        var code;
 
-        function loadLanguage(data) {
-            langText = JSON.parse(data);
 
-        }
-        var rsrcURI = '../rsrc/';
-        var langPrefix = 'language_';
-
-        var langURI = rsrcURI + langPrefix + code + '.json';
-        APP.net.peticion(langURI, false, null, loadLanguage);
-        text[code] = langText;
-        var getText = function (key) {
-            var code = APP.language.code;
-            return APP.language.text[code][key];
+        var setCode=function(cod){
+            code=cod;
         };
+        var loadLanguage = function (cod) {
+            var langPrefix = 'language_';
+            var langObj = langPrefix + code;
+            try {
+                var txt = eval(langObj);
+            } catch (e) {
+                //Si no existe cargar idioma por defecto
+                if (e instanceof ReferenceError) {
+                    code = 'es';
+                    langObj = langPrefix + code;
+                    try {
+                        txt = eval(langObj);
+                    } catch (e) {
 
+                        if (e instanceof ReferenceError) {
+                            console.log('i18n: Error No se ha cargado ningun fichero de idioma');
+                            return -1;
+                        } else {
+                            console.log('i18n: Error desconocido ' + e)
+                        }
+                    }
+                } else {
+                    console.log('i18n: Error desconocido ' + e);
+
+
+                }
+            }
+            text[code] = txt;
+        }
+
+        var getText = function (key) {
+            return text[code][key];
+        };
+        var cod = navigator.language;
+        console.log("navigator.language: " + cod);
+        setCode(cod);
+        loadLanguage(cod);
         console.log("i18n loaded");
 
 
         return {
 
-            text:text,
+            text: text,
             code: code,
             getText: getText,
+            loadLanguage:loadLanguage,
+            setCode:setCode
 
         };
 
