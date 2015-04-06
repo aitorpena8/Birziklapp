b3caf9c049d9404505f7576b44b7a2e560e9d04e
Sp4iK <ospaiko@gmail.com>
2015-04-06 20:30:04 +0200

    Fixed scale of buttons and added text to them.

--- js/app/Menu.js
+++ js/app/Menu.js
@@ -2,6 +2,8 @@ function Menu()
 {
     PIXI.DisplayObjectContainer.call(this);
 
+    var textStyle  = {font: "bold 40px Arvo", fill: "#3e1707", align: "center", stroke: "#a4410e", strokeThickness: 4};
+
     this.logo = new PIXI.Sprite(PIXI.Texture.fromImage("resources/images/sprites/title.png"));
     this.logo.anchor.x = this.logo.anchor.y = 0.5;
     this.logo.position.x = mWidth / 2;
@@ -14,14 +16,20 @@ function Menu()
     this.infoButton         = new PIXI.Sprite(PIXI.Texture.fromImage("resources/images/sprites/buttons/info_128.png"));
     this.optionsButton      = new PIXI.Sprite(PIXI.Texture.fromImage("resources/images/sprites/buttons/options_128.png"));
 
+    this.startButtonText      = new PIXI.Text("PLAY!", textStyle);
+    this.statisticsButtonText = new PIXI.Text("SCORE", textStyle);
+    this.helpButtonText       = new PIXI.Text("HELP", textStyle);
+
     this.startButton.anchor = this.statisticsButton.anchor = this.helpButton.anchor = this.infoButton.anchor = this.optionsButton.anchor = new PIXI.Point(0.5, 0.5);
+    this.startButtonText.anchor = this.statisticsButtonText.anchor = this.helpButtonText.anchor = new PIXI.Point(0.5,0.25);
 
-    //this.startButton.anchor.x = this.startButton.anchor.y = this.statisticsButton.anchor.x = this.statisticsButton.anchor.y = this.helpButton.anchor.x = this.helpButton.anchor.y = 0.5;
+    this.startButton.addChild(this.startButtonText);
+    this.statisticsButton.addChild(this.statisticsButtonText);
+    this.helpButton.addChild(this.helpButtonText);
 
-
-    this.startButton.scale = new PIXI.Point(1,1);
-    this.statisticsButton.scale = this.helpButton.scale = new PIXI.Point(0.8,0.8);
-    this.infoButton.scale = this.optionsButton.scale = new PIXI.Point(0.5,0.5);
+    this.startButton.scale = new PIXI.Point(0.8,0.8);
+    this.statisticsButton.scale = this.helpButton.scale = new PIXI.Point(0.6,0.6);
+    this.infoButton.scale = this.optionsButton.scale = new PIXI.Point(0.4,0.4);
 
     this.statisticsButton.position.x = mWidth * 0.25;
     this.startButton.position.x      = mWidth * 0.5;
