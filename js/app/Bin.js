8f1f0a44a97d5c25936ad62187788f6ef7c0c944
Sp4iK <ospaiko@gmail.com>
2015-04-07 00:04:53 +0200

    "mouseover" added to containers

--- js/app/Bin.js
+++ js/app/Bin.js
@@ -1,12 +1,17 @@
-function Bin(x, y, imageSource, id)
+function Bin(x, y, image1, image2, id)
 {
-    var texture = PIXI.Texture.fromImage(imageSource);
-    PIXI.Sprite.call(this, texture);
+    var texture1 = PIXI.Texture.fromImage(image1);
+    var texture2 = PIXI.Texture.fromImage(image2);
+    PIXI.Sprite.call(this, texture1);
     this.position.x = x;
     this.position.y = y;
     this.id         = id;
 
     this.scale.set(0.4,0.4);
+
+    this.interactive = true;
+    this.mouseover = function(data) {console.log("mouse over");this.setTexture(texture2);};
+    this.mouseout = function(data) {console.log("mouse out");this.setTexture(texture1);};
 
     bins.push(this);
 }
--- js/app/BinsContainer.js
+++ js/app/BinsContainer.js
@@ -8,11 +8,11 @@ function BinsContainer(x, y, width, height)
 
     var offset = 0;
 
-    this.yellow  = new Bin(0, offset, "resources/images/sprites/containers/yellow_256.png", RecycleBins.YELLOW);
-    this.blue    = new Bin(0, height/5 + offset, "resources/images/sprites/containers/blue_256.png", RecycleBins.BLUE);
-    this.green   = new Bin(0, (height/5 + offset) * 2, "resources/images/sprites/containers/green_256.png", RecycleBins.GREEN);
-    this.brown   = new Bin(0, (height/5 + offset) * 3, "resources/images/sprites/containers/brown_256.png", RecycleBins.BROWN);
-    this.default = new Bin(0, (height/5 + offset) * 4, "resources/images/sprites/containers/default_256.png", RecycleBins.DEFAULT);
+    this.yellow  = new Bin(0, offset, "resources/images/sprites/containers/yellow_256.png","resources/images/sprites/containers/yellow_open.png", RecycleBins.YELLOW);
+    this.blue    = new Bin(0, height/5 + offset, "resources/images/sprites/containers/blue_256.png","resources/images/sprites/containers/blue_open.png", RecycleBins.BLUE);
+    this.green   = new Bin(0, (height/5 + offset) * 2, "resources/images/sprites/containers/green_256.png","resources/images/sprites/containers/green_open.png", RecycleBins.GREEN);
+    this.brown   = new Bin(0, (height/5 + offset) * 3, "resources/images/sprites/containers/brown_256.png","resources/images/sprites/containers/brown_open.png", RecycleBins.BROWN);
+    this.default = new Bin(0, (height/5 + offset) * 4, "resources/images/sprites/containers/default_256.png","resources/images/sprites/containers/default_open.png", RecycleBins.DEFAULT);
 
     this.addChild(this.yellow);
     this.addChild(this.blue);
